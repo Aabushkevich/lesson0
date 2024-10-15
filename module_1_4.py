517d2f850651892518c9ad792410c232cbca9749
Aabushkevich <abushkevich9063@gmail.com>
2024-10-15 21:17:58 +0700

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,16 @@
+my_string = input('')
+print(len(my_string)) #Погуглил, каким методом считается количество символов в строке;)
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0:1])
+print(my_string[-1:])
+
+
+
+
+
+
+
+
+
